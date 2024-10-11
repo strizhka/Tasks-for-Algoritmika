4e17143652a2359c6af8fe0935741e6e68e25e97
Daria <dashakoshelek@gmail.com>
2024-10-11 14:50:40 +0300

    Init

---
+++ Assets/Scripts/Crickets.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log("crickets");      // Shows "crickets" every frame
+    }
+}
